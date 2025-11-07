00c41801c6d1a6bcda3eacbd2d5c7463acb35803
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2025-11-07 21:59:01 +0000

    Added complementary VIL instructions

--- contracts/devil/src/program.rs
+++ contracts/devil/src/program.rs
@@ -115,42 +115,47 @@ where
 // STV AssignedAssetQuantities Index 3
 //
 
-const OP_LDL: u128 = 1;
-const OP_LDV: u128 = 2;
-const OP_LDS: u128 = 3;
-const OP_LDD: u128 = 4;
-const OP_LDR: u128 = 5;
-const OP_LDVS: u128 = 6;
-const OP_STL: u128 = 7;
-const OP_STV: u128 = 8;
-const OP_STS: u128 = 9;
-const OP_STR: u128 = 10;
-const OP_STVS: u128 = 11;
-const OP_ADDV: u128 = 12;
-const OP_SUBV: u128 = 13;
-const OP_MULV: u128 = 14;
-const OP_MULS: u128 = 15;
-const OP_DIVV: u128 = 16;
-const OP_DIVS: u128 = 17;
-const OP_SQRTV: u128 = 18;
-const OP_SQRTS: u128 = 19;
-const OP_SUM: u128 = 20;
-const OP_MIN: u128 = 21;
-const OP_MAX: u128 = 22;
-const OP_MINV: u128 = 23;
-const OP_MAXV: u128 = 24;
-const OP_LABUNION: u128 = 25;
-const OP_VZERO: u128 = 26;
-const OP_VONE: u128 = 27;
-const OP_PUSHS: u128 = 28;
-const OP_PUSHL: u128 = 29;
-const OP_PUSHSV: u128 = 30;
-const OP_PUSHLV: u128 = 31;
-const OP_POP: u128 = 32;
-const OP_SWAP: u128 = 33;
-const OP_JADD: u128 = 34;
-const OP_B: u128 = 35;
-const OP_FOLD: u128 = 36;
+const OP_LDL: u128 = 1; // Load labels from VIO
+const OP_LDV: u128 = 2; // Load vector from VIO
+const OP_LDS: u128 = 3; // Load scalar from VIO
+const OP_LDD: u128 = 4; // Load duplicate from stack
+const OP_LDR: u128 = 5; // Load duplicate from registry
+const OP_PKV: u128 = 6; // Pack values from stack into vector
+const OP_PKL: u128 = 7; // Pack values from stack into labels
+const OP_STL: u128 = 8; // Store labels into VIO
+const OP_STV: u128 = 9; // Store vector into VIO
+const OP_STS: u128 = 10; // Store scalar into VIO
+const OP_STR: u128 = 11; // Store into registry
+const OP_UNPK: u128 = 12; // Unpack values into stack
+const OP_ADDV: u128 = 13; // Add vectors
+const OP_ADD: u128 = 14; // Add scalar
+const OP_SUBV: u128 = 15; // Subtract vectors
+const OP_SUB: u128 = 16; // Subtract scalar
+const OP_MULV: u128 = 17; // Multiply vectors component-wise
+const OP_MUL: u128 = 18; // Multiply by scalar
+const OP_DIVV: u128 = 19; // Divide vectors component-wise
+const OP_DIV: u128 = 20; // Divide by scalar
+const OP_SQRTV: u128 = 21; // Component-wise vector square root
+const OP_SQRT: u128 = 22; // Scalar square root
+const OP_SUM: u128 = 23; // Sum of vector components
+const OP_MIN: u128 = 24; // Min of vector components
+const OP_MAX: u128 = 25; // Max of vector components
+const OP_MINV: u128 = 26; // Component-wise vector min
+const OP_MAXV: u128 = 27; // Component-wise vector max
+const OP_LUNN: u128 = 28; // Union of labels
+const OP_ZEROS: u128 = 29; // Vector of zeros matching length of labels
+const OP_ONES: u128 = 30; // Vector of ones matching length of labels
+const OP_IMMS: u128 = 31; // Push immediate scalar on stack
+const OP_IMML: u128 = 32; // Push immediate label on stack
+const OP_VPUSH: u128 = 33; // Push scalar into vector
+const OP_LPUSH: u128 = 34; // Push label into labels
+const OP_VPOP: u128 = 35; // Pop scalar from vector
+const OP_LPOP: u128 = 36; // Pop label from labels
+const OP_POP: u128 = 37; // Pop values from stack
+const OP_SWAP: u128 = 38; // Swap values on stack
+const OP_JADD: u128 = 39; // Left outer join values add using labels
+const OP_B: u128 = 40; // Branch unconditionally
+const OP_FOLD: u128 = 41; // Fold over elements
 
 enum Operand {
     None,
@@ -224,7 +229,7 @@ impl Stack {
         Ok(())
     }
 
-    fn ldvs(&mut self, count: usize) -> Result<(), ErrorCode> {
+    fn pkv(&mut self, count: usize) -> Result<(), ErrorCode> {
         let pos = self
             .stack
             .len()
@@ -244,13 +249,38 @@ impl Stack {
         Ok(())
     }
 
-    fn stvs(&mut self, pos: usize) -> Result<(), ErrorCode> {
-        let v = self.stack.get(pos).ok_or_else(|| ErrorCode::OutOfRange)?;
+    fn pkl(&mut self, count: usize) -> Result<(), ErrorCode> {
+        let pos = self
+            .stack
+            .len()
+            .checked_sub(count)
+            .ok_or_else(|| ErrorCode::StackUnderflow)?;
+
+        let mut res = Labels::new();
+        for v in self.stack.drain(pos..) {
+            match v {
+                Operand::Label(x) => {
+                    res.data.push(x);
+                }
+                _ => Err(ErrorCode::InvalidOperand)?,
+            }
+        }
+        self.push(Operand::Labels(res));
+        Ok(())
+    }
+
+    fn unpk(&mut self) -> Result<(), ErrorCode> {
+        let v = self.stack.pop().ok_or_else(|| ErrorCode::StackUnderflow)?;
         let mut exp = Vec::new();
         match v {
             Operand::Vector(v) => {
-                for x in &v.data {
-                    exp.push(Operand::Scalar(*x));
+                for x in v.data {
+                    exp.push(Operand::Scalar(x));
+                }
+            }
+            Operand::Labels(v) => {
+                for x in v.data {
+                    exp.push(Operand::Label(x));
                 }
             }
             _ => {
@@ -304,6 +334,31 @@ impl Stack {
         }
         Ok(())
     }
+    
+    fn add(&mut self, pos: usize) -> Result<(), ErrorCode> {
+        let (v1, rest) = self
+            .stack
+            .split_last_mut()
+            .ok_or_else(|| ErrorCode::StackUnderflow)?;
+        let v2 = rest.get(pos).ok_or_else(|| ErrorCode::OutOfRange)?;
+        match (v1, v2) {
+            (Operand::Vector(ref mut v1), Operand::Scalar(ref x2)) => {
+                for i in 0..v1.data.len() {
+                    let x1 = &mut v1.data[i];
+                    *x1 = x1.checked_add(*x2).ok_or_else(|| ErrorCode::MathOverflow)?;
+                }
+            }
+            (Operand::Scalar(ref mut x1), Operand::Scalar(ref x2)) => {
+                *x1 = (*x1)
+                    .checked_add(*x2)
+                    .ok_or_else(|| ErrorCode::MathOverflow)?;
+            }
+            _ => {
+                Err(ErrorCode::InvalidOperand)?;
+            }
+        }
+        Ok(())
+    }
 
     fn subv(&mut self, pos: usize) -> Result<(), ErrorCode> {
         let last_index = self
@@ -343,6 +398,31 @@ impl Stack {
                 _ => {
                     Err(ErrorCode::InvalidOperand)?;
                 }
+            }
+        }
+        Ok(())
+    }
+
+    fn sub(&mut self, pos: usize) -> Result<(), ErrorCode> {
+        let (v1, rest) = self
+            .stack
+            .split_last_mut()
+            .ok_or_else(|| ErrorCode::StackUnderflow)?;
+        let v2 = rest.get(pos).ok_or_else(|| ErrorCode::OutOfRange)?;
+        match (v1, v2) {
+            (Operand::Vector(ref mut v1), Operand::Scalar(ref x2)) => {
+                for i in 0..v1.data.len() {
+                    let x1 = &mut v1.data[i];
+                    *x1 = x1.checked_sub(*x2).ok_or_else(|| ErrorCode::MathOverflow)?;
+                }
+            }
+            (Operand::Scalar(ref mut x1), Operand::Scalar(ref x2)) => {
+                *x1 = (*x1)
+                    .checked_sub(*x2)
+                    .ok_or_else(|| ErrorCode::MathOverflow)?;
+            }
+            _ => {
+                Err(ErrorCode::InvalidOperand)?;
             }
         }
         Ok(())
@@ -392,7 +472,7 @@ impl Stack {
         Ok(())
     }
 
-    fn muls(&mut self, pos: usize) -> Result<(), ErrorCode> {
+    fn mul(&mut self, pos: usize) -> Result<(), ErrorCode> {
         let (v1, rest) = self
             .stack
             .split_last_mut()
@@ -460,7 +540,7 @@ impl Stack {
         Ok(())
     }
 
-    fn divs(&mut self, pos: usize) -> Result<(), ErrorCode> {
+    fn div(&mut self, pos: usize) -> Result<(), ErrorCode> {
         let (v1, rest) = self
             .stack
             .split_last_mut()
@@ -502,7 +582,7 @@ impl Stack {
         Ok(())
     }
 
-    fn sqrts(&mut self) -> Result<(), ErrorCode> {
+    fn sqrt(&mut self) -> Result<(), ErrorCode> {
         let v1 = self
             .stack
             .last_mut()
@@ -618,7 +698,7 @@ impl Stack {
         Ok(())
     }
 
-    fn labunion(&mut self, other_labels_pos: usize) -> Result<(), ErrorCode> {
+    fn lunn(&mut self, other_labels_pos: usize) -> Result<(), ErrorCode> {
         let current_len = self.stack.len();
         if current_len < 1 {
             return Err(ErrorCode::StackUnderflow);
@@ -713,7 +793,7 @@ impl Stack {
         Ok(())
     }
 
-    fn vzero(&mut self, labels_pos: usize) -> Result<(), ErrorCode> {
+    fn zeros(&mut self, labels_pos: usize) -> Result<(), ErrorCode> {
         let current_len = self.stack.len();
         if current_len == 0 {
             return Err(ErrorCode::StackUnderflow);
@@ -746,7 +826,7 @@ impl Stack {
         Ok(())
     }
 
-    fn vone(&mut self, labels_pos: usize) -> Result<(), ErrorCode> {
+    fn ones(&mut self, labels_pos: usize) -> Result<(), ErrorCode> {
         let current_len = self.stack.len();
         if current_len == 0 {
             return Err(ErrorCode::StackUnderflow);
@@ -776,17 +856,17 @@ impl Stack {
         Ok(())
     }
 
-    fn pushs(&mut self, value: u128) -> Result<(), ErrorCode> {
+    fn imms(&mut self, value: u128) -> Result<(), ErrorCode> {
         self.push(Operand::Scalar(Amount::from_u128_raw(value)));
         Ok(())
     }
 
-    fn pushl(&mut self, value: u128) -> Result<(), ErrorCode> {
+    fn imml(&mut self, value: u128) -> Result<(), ErrorCode> {
         self.push(Operand::Label(value));
         Ok(())
     }
 
-    fn pushsv(&mut self, value: u128) -> Result<(), ErrorCode> {
+    fn vpush(&mut self, value: u128) -> Result<(), ErrorCode> {
         let v = self
             .stack
             .last_mut()
@@ -800,7 +880,7 @@ impl Stack {
         Ok(())
     }
 
-    fn pushlv(&mut self, value: u128) -> Result<(), ErrorCode> {
+    fn lpush(&mut self, value: u128) -> Result<(), ErrorCode> {
         let v = self
             .stack
             .last_mut()
@@ -808,6 +888,36 @@ impl Stack {
         match v {
             Operand::Labels(ref mut v) => {
                 v.data.push(value);
+            }
+            _ => Err(ErrorCode::InvalidOperand)?,
+        }
+        Ok(())
+    }
+
+    fn vpop(&mut self) -> Result<(), ErrorCode> {
+        let v = self
+            .stack
+            .last_mut()
+            .ok_or_else(|| ErrorCode::StackUnderflow)?;
+        match v {
+            Operand::Vector(ref mut v) => {
+                let val = v.data.pop().ok_or_else(|| ErrorCode::OutOfRange)?;
+                self.stack.push(Operand::Scalar(val));
+            }
+            _ => Err(ErrorCode::InvalidOperand)?,
+        }
+        Ok(())
+    }
+
+    fn lpop(&mut self) -> Result<(), ErrorCode> {
+        let v = self
+            .stack
+            .last_mut()
+            .ok_or_else(|| ErrorCode::StackUnderflow)?;
+        match v {
+            Operand::Labels(ref mut v) => {
+                let val = v.data.pop().ok_or_else(|| ErrorCode::OutOfRange)?;
+                self.stack.push(Operand::Label(val));
             }
             _ => Err(ErrorCode::InvalidOperand)?,
         }
@@ -1039,51 +1149,64 @@ where
                     pc += 1;
                     stack.op_str(reg)?;
                 }
-                OP_LDVS => {
+                OP_PKV => {
                     let count = code[pc] as usize;
                     pc += 1;
-                    stack.ldvs(count)?;
+                    stack.pkv(count)?;
                 }
-                OP_STVS => {
-                    let pos = code[pc] as usize;
+                OP_PKL => {
+                    let count = code[pc] as usize;
                     pc += 1;
-                    stack.stvs(pos)?;
+                    stack.pkl(count)?;
+                }
+                OP_UNPK => {
+                    stack.unpk()?;
                 }
                 OP_ADDV => {
                     let pos = code[pc] as usize;
                     pc += 1;
                     stack.addv(pos)?;
                 }
+                OP_ADD => {
+                    let pos = code[pc] as usize;
+                    pc += 1;
+                    stack.add(pos)?;
+                }
                 OP_SUBV => {
                     let pos = code[pc] as usize;
                     pc += 1;
                     stack.subv(pos)?;
+                }
+                OP_SUB => {
+                    let pos = code[pc] as usize;
+                    pc += 1;
+                    stack.sub(pos)?;
                 }
                 OP_MULV => {
                     let pos = code[pc] as usize;
                     pc += 1;
                     stack.mulv(pos)?;
                 }
-                OP_MULS => {
+                OP_MUL => {
                     let pos = code[pc] as usize;
                     pc += 1;
-                    stack.muls(pos)?;
+                    stack.mul(pos)?;
                 }
                 OP_DIVV => {
                     let pos = code[pc] as usize;
                     pc += 1;
                     stack.divv(pos)?;
                 }
-                OP_DIVS => {
+                OP_DIV => {
                     let pos = code[pc] as usize;
                     pc += 1;
-                    stack.divs(pos)?;
+                    stack.div(pos)?;
                 }
                 OP_SQRTV => {
                     stack.sqrtv()?;
                 }
-                OP_SQRTS => {
-                    stack.sqrts()?;
+                OP_SQRT => {
+                    stack.sqrt()?;
                 }
                 OP_SUM => {
                     stack.sum()?;
@@ -1104,40 +1227,46 @@ where
                     pc += 1;
                     stack.maxv(pos)?;
                 }
-                OP_LABUNION => {
+                OP_LUNN => {
                     let pos = code[pc] as usize;
                     pc += 1;
-                    stack.labunion(pos)?;
+                    stack.lunn(pos)?;
                 }
-                OP_VZERO => {
+                OP_ZEROS => {
                     let pos = code[pc] as usize;
                     pc += 1;
-                    stack.vzero(pos)?;
+                    stack.zeros(pos)?;
                 }
-                OP_VONE => {
+                OP_ONES => {
                     let pos = code[pc] as usize;
                     pc += 1;
-                    stack.vone(pos)?;
+                    stack.ones(pos)?;
                 }
-                OP_PUSHS => {
+                OP_IMMS => {
                     let val = code[pc];
                     pc += 1;
-                    stack.pushs(val)?;
+                    stack.imms(val)?;
                 }
-                OP_PUSHL => {
+                OP_IMML => {
                     let val = code[pc];
                     pc += 1;
-                    stack.pushl(val)?;
+                    stack.imml(val)?;
                 }
-                OP_PUSHSV => {
+                OP_VPUSH => {
                     let val = code[pc];
                     pc += 1;
-                    stack.pushsv(val)?;
+                    stack.vpush(val)?;
                 }
-                OP_PUSHLV => {
+                OP_LPUSH => {
                     let val = code[pc];
                     pc += 1;
-                    stack.pushlv(val)?;
+                    stack.lpush(val)?;
+                }
+                OP_VPOP => {
+                    stack.vpop()?;
+                }
+                OP_LPOP => {
+                    stack.lpop()?;
                 }
                 OP_POP => {
                     let count = code[pc] as usize;
